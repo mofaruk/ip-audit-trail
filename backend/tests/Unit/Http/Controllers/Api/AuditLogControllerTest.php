9540254f43708fcd11f7e5025853f2f8a039658f
Omar <omar.faruk.ruet@gmail.com>
2024-09-16 09:46:30 +0600

    Add Unit tests for AuditLogController Controller

---
+++ backend/tests/Unit/Http/Controllers/Api/AuditLogControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Unit\Http\Controllers\Api;
+
+use PHPUnit\Framework\TestCase;
+
+class AuditLogControllerTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
